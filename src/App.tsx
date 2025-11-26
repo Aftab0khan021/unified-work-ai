d76d6e6a2272bd5cd48ca88733db44806cb5f80c
Aftab Pathan <paftab320@gmail.com>
2025-11-27 03:24:35 +0530

    initial 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,14 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/chat" element={<Chat />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-          <Route path="/tasks" element={<Tasks />} />
-        </Routes>
+  <Route path="/" element={<Index />} />
+  <Route path="/auth" element={<Auth />} />
+  <Route path="/chat" element={<Chat />} />
+  <Route path="/tasks" element={<Tasks />} /> {/* Move this UP */}
+
+  {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+  <Route path="*" element={<NotFound />} />
+</Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
